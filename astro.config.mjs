fe0562e2f2b7811fa744fde11401e5b0a4095545
José Tanicuchí <josluistanic99@outlook.es>
2023-11-19 19:23:45 -0500

    Update site in astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://astro.josluistanic.com"
+    site: "https://astro-269.netlify.app"
 });
